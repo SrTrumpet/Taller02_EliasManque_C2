9b24f2b68d336f724231b8e93c1efdf424f48c01
Elias Manque <elias.manque@alumnos.ucn.cl>
2022-06-23 10:53:31 -0400

    Update

--- src/Logica/procesoRegistro.java
+++ src/Logica/procesoRegistro.java
@@ -1,8 +1,24 @@
 package Logica;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class procesoRegistro {
 	
-	public procesoRegistro(String usuario, String fullName, String correo, String contacto, String pass, String passConfirm) {
+	public boolean ResgistroUsuario(String usuario, String fullName, String correo, String contacto, String pass, String passConfirm,File archUsuario) throws IOException {
+		
+		if(!pass.equals(passConfirm)) {
+			return false;
+		}else {
+			String ruta = archUsuario.getAbsolutePath();
+	        FileWriter arch = new FileWriter(ruta);
+			
+			return true;
+		}
+		
+		
+		
 		
 		
 	}
